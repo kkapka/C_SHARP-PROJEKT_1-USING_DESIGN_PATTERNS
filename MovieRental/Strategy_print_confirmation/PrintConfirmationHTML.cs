f46c1d78b65cd621bb779ee56af9a671f083d5ad
Kamil Kapka <kamil.kapka@hotmail.com>
2016-11-18 00:54:02 +0100

    problem with writing file ...

--- MovieRental/Strategy_print_confirmation/PrintConfirmationHTML.cs
+++ MovieRental/Strategy_print_confirmation/PrintConfirmationHTML.cs
@@ -8,27 +8,29 @@ namespace MovieRental.Strategy_print_confirmation
         public void print(MovieRental rental)
         {
             RentalPriceContext rcontext = new RentalPriceContext(new RentalPriceA());
-            string fileName= "./rental_confirmation.html";
-            FileStream f = new FileStream(fileName,FileMode.OpenOrCreate,FileAccess.Write);
-            StreamWriter file = new StreamWriter(f);
+            string fileName = "rental_confirmation.html";
 
-            file.WriteLine("<html>");
-            file.WriteLine("<head>");
-            file.WriteLine("</head>");
-            file.WriteLine("<body>");
-            file.WriteLine("<table>");
-            file.WriteLine("<tr><td>Name: " + rental.Customer.Name + "</td><td>Surname: " + rental.Customer.Surname+"</td></tr>");
-            file.WriteLine("<tr><td>Amount of rent days: " + (int)(rental.ReturnDate - rental.RentDate).TotalDays + "</td></tr>");
-            file.WriteLine("<tr><td>Genre: " + rental.Movie.Category.Name + "</td><td>Title: " + rental.Movie.Name + "</td></tr>");
-            file.WriteLine("<tr><td>Total cost: " + rcontext.executeStrategy(rental)+ "</td></tr>");
-            file.WriteLine("<tr><td>Number of loyalty points: " + rental.Customer.LoyaltyPoints+ "</td></tr>");
-            file.WriteLine("</table>");
-            file.WriteLine("</body>");
-            file.WriteLine("</html>");
+            using (FileStream fileStream=File.Create(fileName))
+            {
+                using (StreamWriter file=new StreamWriter(fileStream))
+                {
+                    file.WriteLine("<html>");
+                    file.WriteLine("<head>");
+                    file.WriteLine("</head>");
+                    file.WriteLine("<body>");
+                    file.WriteLine("<table>");
+                    file.WriteLine("<tr><td>Name: " + rental.Customer.Name + "</td><td>Surname: " + rental.Customer.Surname + "</td></tr>");
+                    file.WriteLine("<tr><td>Amount of rent days: " + (int)(rental.ReturnDate - rental.RentDate).TotalDays + "</td></tr>");
+                    file.WriteLine("<tr><td>Genre: " + rental.Movie.Category.Name + "</td><td>Title: " + rental.Movie.Name + "</td></tr>");
+                    file.WriteLine("<tr><td>Total cost: " + rcontext.executeStrategy(rental) + "</td></tr>");
+                    file.WriteLine("<tr><td>Number of loyalty points: " + rental.Customer.LoyaltyPoints + "</td></tr>");
+                    file.WriteLine("</table>");
+                    file.WriteLine("</body>");
+                    file.WriteLine("</html>");
 
-            file.Close();
-
-            System.Diagnostics.Process.Start("CMD.exe", fileName);
+                    file.Close();
+                }
+            }
         }
     }
 }
