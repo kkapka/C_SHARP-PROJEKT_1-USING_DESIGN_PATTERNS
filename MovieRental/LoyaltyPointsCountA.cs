b6d02d70028d1a565ff01105743555ad65c7ff5f
Kamil Kapka <kamil.kapka@hotmail.com>
2016-11-17 21:44:22 +0100

    Ended count loyalty points strategy

--- MovieRental/LoyaltyPointsCountA.cs
+++ MovieRental/LoyaltyPointsCountA.cs
@@ -1,10 +1,30 @@
-﻿namespace MovieRental
+﻿using System;
+
+namespace MovieRental
 {
     class LoyaltyPointsCountA : LoyaltyPointsCountStrategy
     {
         public void count(MovieRental rental)
         {
-            
+            int daysCount = (int)(rental.ReturnDate - rental.RentDate).TotalDays;
+            int points;
+            string categoryName = rental.Movie.Category.Name;
+
+            if (daysCount > 0)
+            {
+                points = (daysCount - 1) * 1 + 1 * 5;
+
+                if (categoryName == "Nowość")
+                {
+                    points *= 2;
+                }
+
+                rental.Customer.LoyaltyPoints = points;
+            }
+            else
+            {
+                Console.WriteLine("Dates difference is negative ! ");
+            }
         }
     }
 }
