3cf7ba96fcccf36947e6562892d842792f1288af
Kamil Kapka <kamil.kapka@hotmail.com>
2016-11-17 22:02:59 +0100

    small test

--- MovieRental/Program.cs
+++ MovieRental/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace MovieRental
+﻿using System;
+
+namespace MovieRental
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Customer c = new Customer("Kamil", "Kapka");
+            MovieCategory mc = new MovieCategory("Nowość");
+            Movie m = new Movie("Iluzja 2", mc);
+            MovieRental mr = new MovieRental(c, m, DateTime.Now, DateTime.Now.AddDays(7));
 
+            LoyaltyPointsCountContext context = new LoyaltyPointsCountContext(new LoyaltyPointsCountA());
+            context.executeStrategy(mr);
+
+            Console.WriteLine(mr.Customer.LoyaltyPoints);
+            Console.ReadLine();
         }
     }
 }
