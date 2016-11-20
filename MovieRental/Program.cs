61ffd6315bb9f98cdcc97f83d202311bc01db963
Kamil Kapka <kamil.kapka@hotmail.com>
2016-11-20 01:24:31 +0100

    Ended GUI

--- MovieRental/Program.cs
+++ MovieRental/Program.cs
@@ -8,27 +8,10 @@ namespace MovieRental
     {
         static void Main(string[] args)
         {
-            /*
-            Customer c = new Customer("Kamil", "Kapka");
-            MovieCategory mc = new MovieCategory("New");
-            Movie m = new Movie("Iluzja 2", mc);
-
-            MovieRental mr = new MovieRental(c, m, DateTime.Now, DateTime.Now.AddDays(7));
-
-            LoyaltyPointsCountContext context = new LoyaltyPointsCountContext(new LoyaltyPointsCountA());
-            context.executeStrategy(mr);
-
-            PrintConfirmationContext pcontext = new PrintConfirmationContext(new PrintConfirmationStandard());
-            pcontext.executeStrategy(mr);
-
-            PrintConfirmationContext pcontext2 = new PrintConfirmationContext(new PrintConfirmationHTML());
-            pcontext2.executeStrategy(mr);*/
 
             RentalManagementSystem rms = new RentalManagementSystem();
             rms.init();
             rms.start();
-
-            Console.ReadLine();
         }
     }
 }
--- MovieRental/rental_management_system/RentalManagementSystemGUI.cs
+++ MovieRental/rental_management_system/RentalManagementSystemGUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieRental.Strategy_print_confirmation;
+using System;
 
 namespace MovieRental.rental_management_system
 {
@@ -33,8 +34,9 @@ namespace MovieRental.rental_management_system
 
             while (optionId != 5)
             {
+                chooseAction(optionId);
                 printMenu();
-                optionId=int.Parse(getTextFromConsole());
+                optionId = int.Parse(getTextFromConsole());
             }
             
         }
@@ -47,10 +49,15 @@ namespace MovieRental.rental_management_system
                     doCase1();
                     break;
                 case 2:
+                    doCase2();
                     break;
                 case 3:
+                    doCase3();
                     break;
                 case 4:
+                    doCase4();
+                    break;
+                default:
                     break;
             }
         }
@@ -71,6 +78,44 @@ namespace MovieRental.rental_management_system
             int movieCategoryId = int.Parse(getTextFromConsole());
 
             addMovie(movieTitle, movieCategoryId);
+        }
+
+        private void doCase3()
+        {
+            printTextOnConsole("Write customer id, movie id and rent length (in days)");
+            int customerId = int.Parse(getTextFromConsole());
+            int movieId = int.Parse(getTextFromConsole());
+            int rentLength = int.Parse(getTextFromConsole());
+            addMovieRental(customerId, movieId, rentLength);
+        }
+
+        private void doCase4()
+        {
+            printTextOnConsole("Choose rent: ");
+            int rentId = int.Parse(getTextFromConsole());
+            printTextOnConsole(
+                @"Choose confirmation print format:
+                1. Standard
+                2. HTML
+                ");
+
+            int formatId = int.Parse(getTextFromConsole());
+
+            PrintConfirmationContext context=null;
+
+            switch (formatId)
+            {
+                case 1:
+                    context = new PrintConfirmationContext(new PrintConfirmationStandard());
+                    break;
+                case 2:
+                    context = new PrintConfirmationContext(new PrintConfirmationHTML());
+                    break;
+
+            }
+
+            context.executeStrategy(movieRentalList[rentId]);
+
         }
     }
 }
